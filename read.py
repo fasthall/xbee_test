ad18e2cfc7f73beaf4fdbba278777da42477c112
Wei-Tsung Lin <fasthall@gmail.com>
2017-01-30 16:04:00 -0800

    for python2

--- read.py
+++ read.py
@@ -3,7 +3,7 @@ import sys
 
 if __name__ == '__main__':
 	port = sys.argv[1]
-	msg = sys.argv[2]
 	s = serial.Serial(port, 9600)
-	#s.write(list(bytearray(msg, 'utf-8')))
-	s.write(msg)
+	while True:
+		s.read()
+
